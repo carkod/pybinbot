fd2a15b9626f26c45eeca11ceb56259e27d9637f
Carlos Wu Fei <carkodw@gmail.com>
2026-01-11 17:10:15 +0000

    Add missing method to convert interval millseconds for Binance

--- pybinbot/shared/enums.py
+++ pybinbot/shared/enums.py
@@ -172,6 +172,27 @@ class BinanceKlineIntervals(str, Enum):
         }
         return interval_map.get(self.value, self.value)
 
+    def get_interval_ms(interval_str: str) -> int:
+        """Convert Binance interval string to milliseconds"""
+        interval_map = {
+            "1m": 60 * 1000,
+            "3m": 3 * 60 * 1000,
+            "5m": 5 * 60 * 1000,
+            "15m": 15 * 60 * 1000,
+            "30m": 30 * 1000,
+            "1h": 60 * 60 * 1000,
+            "2h": 2 * 60 * 60 * 1000,
+            "4h": 4 * 60 * 60 * 1000,
+            "6h": 6 * 60 * 60 * 1000,
+            "8h": 8 * 60 * 60 * 1000,
+            "12h": 12 * 60 * 60 * 1000,
+            "1d": 24 * 60 * 60 * 1000,
+            "3d": 3 * 24 * 60 * 60 * 1000,
+            "1w": 7 * 24 * 60 * 60 * 1000,
+            "1M": 30 * 24 * 60 * 60 * 1000,  # Approximate month as 30 days
+        }
+        return interval_map.get(interval_str, 60 * 1000)  # Default to 1 minute
+
 
 class KucoinKlineIntervals(str, Enum):
     ONE_MINUTE = "1min"
