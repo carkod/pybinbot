4f0bd4768d0a005a0693a3f4d061816902333390
Carlos Wu <carkodw@gmail.com>
2026-02-16 00:38:35 +0000

    Allow Kucoin APIs to be imported independently (#40)
    * Centralize futures API initialisation methods
    * Add symbol conversation between exchanges utilities

--- pybinbot/apis/kucoin/rest.py
+++ pybinbot/apis/kucoin/rest.py
@@ -3,6 +3,7 @@ from kucoin_universal_sdk.model import TransportOptionBuilder
 from kucoin_universal_sdk.model import ClientOptionBuilder
 from kucoin_universal_sdk.model import (
     GLOBAL_API_ENDPOINT,
+    GLOBAL_FUTURES_API_ENDPOINT,
 )
 
 
@@ -11,23 +12,55 @@ class KucoinRest:
         self.key = key
         self.secret = secret
         self.passphrase = passphrase
-
-    def setup_client(self) -> DefaultClient:
-        http_transport_option = (
+        self.http_transport_option = (
             TransportOptionBuilder()
             .set_keep_alive(True)
             .set_max_pool_size(10)
             .set_max_connection_per_pool(10)
             .build()
         )
+
+    def setup_client(self) -> DefaultClient:
         client_option = (
             ClientOptionBuilder()
             .set_key(self.key)
             .set_secret(self.secret)
             .set_passphrase(self.passphrase)
             .set_spot_endpoint(GLOBAL_API_ENDPOINT)
-            .set_transport_option(http_transport_option)
+            .set_transport_option(self.http_transport_option)
             .build()
         )
         self.client = DefaultClient(client_option)
         return self.client
+
+    def setup_futures_api(self) -> None:
+        """
+        Separates spot and futures as they have completely different interfaces
+        also creates consistency and resusability of variable
+        and attribute naming.
+
+        The methods here should be mostly fixated
+        not often changed or tweaked.
+
+        :param self: Description
+        """
+        client_option = (
+            ClientOptionBuilder()
+            .set_key(self.key)
+            .set_secret(self.secret)
+            .set_passphrase(self.passphrase)
+            .set_futures_endpoint(GLOBAL_FUTURES_API_ENDPOINT)
+            .set_transport_option(self.http_transport_option)
+            .build()
+        )
+        self.futures_client = DefaultClient(client_option)
+        self.futures_account_api = (
+            self.futures_client.rest_service().get_account_service().get_account_api()
+        )
+        self.transfer_api = (
+            self.futures_client.rest_service().get_account_service().get_transfer_api()
+        )
+        self.futures_service = self.futures_client.rest_service().get_futures_service()
+        self.futures_market_api = self.futures_service.get_market_api()
+        self.futures_order_api = self.futures_service.get_order_api()
+        self.futures_positions_api = self.futures_service.get_positions_api()
---
+++ pybinbot/shared/utils.py
@@ -0,0 +1,24 @@
+from pybinbot.models.bot_base import BotBase, MarketType
+
+
+def convert_to_kucoin_symbol(bot: BotBase) -> str:
+    """
+    Convert symbol to KuCoin format if exchange is KuCoin
+    e.g. BTCUSDC -> BTC-USDC
+    """
+    quote = bot.quote_asset.value
+    base = bot.pair.replace(quote, "")
+    if bot.market_type == MarketType.FUTURES:
+        kucoin_symbol = bot.pair
+    else:
+        kucoin_symbol = f"{base}-{quote}"
+
+    return kucoin_symbol
+
+
+def convert_from_kucoin_symbol(kucoin_symbol: str) -> str:
+    """
+    Convert symbol from KuCoin format to standard format
+    e.g. BTC-USDC -> BTCUSDC
+    """
+    return kucoin_symbol.replace("-", "")
